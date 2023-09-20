c3b4221f0b2d694d886e004a15000a1599706f36
Thitirat Sukijprasert <effthitirat@gmail.com>
2023-09-20 20:40:51 +0700

    Jpacman exercise

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,42 @@
+package nl.tudelft.jpacman.board;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+    private Board board;
+    private Board boardWithNull;
+    final int width = 1;
+    final int height = 1;
+    @BeforeEach
+    void setUp() {
+        Square[][] grid = new Square[width][height];
+        grid[0][0] = new BasicSquare();
+        board = new Board(grid);
+    }
+
+    @Test
+    void testHeight() {
+        assertThat(board.getHeight()).isEqualTo(height);
+    }
+
+    @Test
+    void testWidth() {
+        assertThat(board.getWidth()).isEqualTo(width);
+    }
+
+    @Test
+    void testInvariant() {
+        assertThat(board.invariant()).isTrue();
+    }
+
+    @Test
+    void testNullSquare() {
+        Square[][] gridWithNull = new Square[width][height];
+        gridWithNull[0][0] = null;
+        boardWithNull = new Board(gridWithNull);
+        Square square = boardWithNull.squareAt(0, 0);
+        assertThat(square).isNull();
+    }
+}
--- src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
+++ src/test/java/nl/tudelft/jpacman/board/OccupantTest.java
@@ -32,7 +32,7 @@ class OccupantTest {
     @Test
     void noStartSquare() {
         // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        assertThat(unit.hasSquare()).isFalse();
     }
 
     /**
@@ -42,7 +42,9 @@ class OccupantTest {
     @Test
     void testOccupy() {
         // Remove the following placeholder:
-        assertThat(unit).isNotNull();
+        BasicSquare target = new BasicSquare();
+        unit.occupy(target);
+        assertThat(unit.hasSquare()).isEqualTo(true);
     }
 
     /**
@@ -52,6 +54,11 @@ class OccupantTest {
     @Test
     void testReoccupy() {
         // Remove the following placeholder:
+        Square square1 = new BasicSquare();
+        Square square2 = new BasicSquare();
+        unit.occupy(square1);
+        unit.occupy(square2);
+        assertThat(unit.getSquare()).isEqualTo(square2);
         assertThat(unit).isNotNull();
     }
 }
